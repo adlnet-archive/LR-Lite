25068896b21117d66838522a39ded5a9cbb75e00
Walt Grata <wegrata@gmail.com>
2014-03-25 13:13:37 -0400

    added travis file

--- lrlite/api/tests.py
+++ lrlite/api/tests.py
@@ -163,3 +163,67 @@ class ViewTests(unittest.TestCase):
         })
         info = add_envelope(request)    
         assert info['OK'], str(info)
+
+    def test_add_envelope_linked_fail(self):
+        from .views import add_envelope
+        from requests import post
+        resp = post("http://localhost:5984/_session", data={"name": 'user', "password": 'password'})        
+        request = testing.DummyRequest()        
+        request.db = self.add_couchdb(request)
+        request.auth_cookie = resp.headers['set-cookie']
+        request.node_id = "abc123"
+        request.body = json.dumps({
+            "doc_type": "resource_data",
+            "resource_locator": "",
+            "resource_data": "test",
+            "keys": [],
+            "TOS": {
+                "submission_TOS": "http://www.learningregistry.org/information-assurances/open-information-assurances-1-0"
+            },
+            "resource_data_type": "metadata",
+            "payload_schema_locator": "http://www.w3.org/TR/2012/WD-microdata-20121025/#converting-html-to-other-formats",
+            "payload_placement": "linked",
+            "payload_schema": ["plain text"],
+            "doc_version": "0.23.0",
+            "active": True,
+            "identity": {
+                "submitter": "inBloom Tagger Application <tagger@inbloom.org>",
+                "signer": "Learning Registry SLC Node <lrnode@inbloom.org>",
+                "submitter_type": "user",
+                "curator": "5a4bfe96-1724-4565-9db1-35b3796e3ce1:jordi.juarez@udl.cat@null"
+            }
+        })
+        info = add_envelope(request)    
+        assert not info['OK']
+
+
+    def test_add_envelope_inline_fail(self):
+        from .views import add_envelope
+        from requests import post
+        resp = post("http://localhost:5984/_session", data={"name": 'user', "password": 'password'})        
+        request = testing.DummyRequest()        
+        request.db = self.add_couchdb(request)
+        request.auth_cookie = resp.headers['set-cookie']
+        request.node_id = "abc123"
+        request.body = json.dumps({
+            "doc_type": "resource_data",
+            "resource_locator": "",
+            "keys": [],
+            "TOS": {
+                "submission_TOS": "http://www.learningregistry.org/information-assurances/open-information-assurances-1-0"
+            },
+            "resource_data_type": "metadata",
+            "payload_schema_locator": "http://www.w3.org/TR/2012/WD-microdata-20121025/#converting-html-to-other-formats",
+            "payload_placement": "inline",
+            "payload_schema": ["plain text"],
+            "doc_version": "0.23.0",
+            "active": True,
+            "identity": {
+                "submitter": "inBloom Tagger Application <tagger@inbloom.org>",
+                "signer": "Learning Registry SLC Node <lrnode@inbloom.org>",
+                "submitter_type": "user",
+                "curator": "5a4bfe96-1724-4565-9db1-35b3796e3ce1:jordi.juarez@udl.cat@null"
+            }
+        })
+        info = add_envelope(request)    
+        assert not info['OK']
