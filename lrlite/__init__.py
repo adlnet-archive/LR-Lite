54ef31315336fcab7bcd06fc94cf05bd255b3229
Walt Grata <wegrata@gmail.com>
2014-04-03 10:17:04 -0400

    had to do checking for uwsgi, seems i can't load it unless i run the app in uwsgi

--- lrlite/__init__.py
+++ lrlite/__init__.py
@@ -1,9 +1,64 @@
 from pyramid.config import Configurator
+from pprint import pformat
+import requests
+import multiprocessing
+try:
+    import uwsgi
+except:
+    pass
 from couchdbkit import *
+from couchdbkit.changes import ChangesStream, foreach
 from pyramid.security import remember
 from pyramid.authentication import BasicAuthAuthenticationPolicy
 from pyramid.security import Authenticated, Allow, Everyone, ALL_PERMISSIONS
 from pyramid.authorization import ACLAuthorizationPolicy
+from logging import getLogger
+log = getLogger(__name__)
+
+
+def update_views(db_uri):
+    resp = requests.get(db_uri + '/_all_docs', params={
+        'startkey': '"_design/"',
+        'endkey': '"_design0"'
+    })
+    data = resp.json()
+    for design_doc in data['rows']:
+        doc = requests.get(db_uri + '/' + design_doc['id']).json()
+        for k in doc['views']:
+            view_result = requests.get(
+                db_uri + '/' + design_doc['id'] + '/_view/' + k, params={'stale': 'update_after', "limit": 1}).json()
+
+
+def monitor_changes(db):
+    previous_seq = 0
+    count = 0
+    while True:
+        stream = ChangesStream(db, feed="continuous",
+                               heartbeat=True, since=previous_seq)
+        log.debug("pull from changes")
+        for c in stream:
+            previous_seq = c['seq']
+            count += 1
+            if count % 100 == 0:
+                log.debug("updating views")
+                try:
+                    uwsgi.spool({"action": "update_view", "uri": db.uri})
+                except:
+                    pass
+
+
+def spooler(env):
+    action = env.get('action')
+    if action == 'update_view':
+        update_views(env['uri'])
+        return uwsgi.SPOOL_OK
+    else:
+        log.error("Unknow Task: {0}".format(pformat(env)))
+        return uwsgi.SPOOL_OK
+try:
+    uwsgi.spooler = spooler
+except:
+    pass
 
 
 class Root(object):
@@ -20,8 +75,8 @@ class Root(object):
 
 def auth_check(user, password, req):
     import requests
-    data = {"name": user, "password": password}    
-    response = requests.post(req.db.server_uri + "/_session", data=data)    
+    data = {"name": user, "password": password}
+    response = requests.post(req.db.server_uri + "/_session", data=data)
     data = response.json()
     roles = []
     roles.extend(data.get("roles", []))
@@ -48,13 +103,13 @@ def main(global_config, **settings):
 
     def get_node_id(req):
         return settings['node.id']
-    config.add_request_method(add_couchdb, 
-                              'db', 
-                              reify=True, 
+    config.add_request_method(add_couchdb,
+                              'db',
+                              reify=True,
                               property=True)
-    config.add_request_method(add_users, 
-                              "users", 
-                              reify=True, 
+    config.add_request_method(add_users,
+                              "users",
+                              reify=True,
                               property=True)
     config.add_request_method(get_node_id,
                               'node_id',
@@ -68,5 +123,8 @@ def main(global_config, **settings):
     config.add_route("signout", '/signout')
     config.add_route("userkey", '/user/:username/key')
     config.include('lrlite.api', route_prefix="/v1")
+    p = multiprocessing.Process(target=monitor_changes, args=(
+        config.registry.db.get_or_create_db(settings['couchdb.db']), ))
+    p.start()
     config.scan()
     return config.make_wsgi_app()
